6a9d1f0730ed2920442e5837f4dbdb3e870b916a
Aslan-999 <aslanqehramanovofficial@gmail.com>
2022-09-26 11:09:24 +0400

    Phyton Work

---
+++ homework18.py
@@ -0,0 +1,55 @@
+import time
+from pathlib import Path
+import os
+import uuid
+import json
+
+# user_value = int(input('Geri sayim ucun baslama vaxti teyin edin : '))
+
+
+# for i in range(user_value,-1,-1):
+#     print(i)
+#     time.sleep(1)
+#     if i == 0:
+#         print('Geri sayim bitmisdir')
+        
+
+        
+# current_dir = Path().absolute()
+
+# parents = current_dir.parent.parent
+
+# file_suffix = []
+
+# for i in os.listdir(parents):
+#     sfx = Path(i).absolute().suffix
+#     if sfx != '':
+#         file_suffix.append(sfx)    
+#     else:
+#         pass
+    
+# print(file_suffix)
+
+# sifre = uuid.uuid4()
+
+# print(sifre)
+
+
+
+
+# with open('info.json', 'r') as file:
+#     info_data = json.load(file)
+    
+
+    
+# new_data = []
+    
+# for users in info_data:
+#     sifre = uuid.uuid4()
+#     users['password'] = str(sifre)
+#     new_data.append(users)
+    
+# with open('new_data.json', 'w') as new_file:
+#     json.dump(new_data, new_file)
+    
+
